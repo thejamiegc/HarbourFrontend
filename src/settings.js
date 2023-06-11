a91d46d54b719980a43b46004634cf7920239de8
thejamiegc <jcallan32@gmail.com>
2023-06-11 20:05:02 +0200

    first commit

---
+++ src/settings.js
@@ -0,0 +1,8 @@
+export const baseURL = "http://localhost:8080/quickstart/api";
+
+export const dadJokeURL = "http://localhost:8080/quickstart/api/joke/dad";
+
+export const chuckURL = "http://localhost:8080/quickstart/api/joke/chuck";
+
+export const ultimateJokeURL = "http://localhost:8080/quickstart/api/joke/2jokes";
+
